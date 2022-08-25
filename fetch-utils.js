186a760b6c26271d5e2d33d8de359b2b8d1907b8
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-25 12:35:32 -0500

    Connect to supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://ycrjdcltdpujspwklmtr.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljcmpkY2x0ZHB1anNwd2tsbXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2NDA4ODAsImV4cCI6MTk3NTIxNjg4MH0.09-eHnBOrLeSZ5iozNMkme5G9W9_LfVD2GYU4ycn4eg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
