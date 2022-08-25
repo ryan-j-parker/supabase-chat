dac52e9b2223be7aef7be71bd656836ce8f7fb47
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-25 16:15:37 -0500

    Add placeholder img and username to rendered comments

--- render-utils.js
+++ render-utils.js
@@ -1,8 +1,14 @@
 export function renderComments(comment) {
     const ul = document.createElement('ul');
     const li = document.createElement('li');
+    const avatar = document.createElement('img');
+    const userName = document.createElement('span');
+    
+    userName.textContent = 'placeholder: ';
+    avatar.src = './assets/avatar-placeholder-circle.png';
 
     li.textContent = comment.text;
+    li.prepend(avatar, userName);
 
     ul.append(li);
     
